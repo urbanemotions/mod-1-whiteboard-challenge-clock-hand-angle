175e94a06d887761fed31d3a8ad0d750ef23401e
sin wan wildy <sw.wildy@gmail.com>
2021-01-26 10:02:23 -0800

    commit tree 76565be684cf57a097d225d3cc0af111585f38e8

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,15 @@
 def clock_angle(time)
     # code your solution here
+    hour = time.split(':')[0].to_f
+    minute = time.split(':')[1].to_f
+
+    hour_angle = hour * 30
+    minute_angle = minute * 6 
+
+    minHourCorrect = hour_angle + minute * 0.5
+
+    puts "corret #{minHourCorrect - minute_angle}"
+
 end
+
+#clock_angle('3:15')
